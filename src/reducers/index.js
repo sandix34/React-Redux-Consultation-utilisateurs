13f114bbac7bef1ac01b4e3f08777791b826c1f2
sandrine <sandrinezocca@gmail.com>
2019-05-27 17:02:08 +0200

    création d'un reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,14 @@
+// Fichier de configuration des Reducers où on associe le retrour d'un Reducer 
+// à une variable qui sera une variable du state de l'application
+
 import { combineReducers } from 'redux';
+import UsersReducer from './reducer_users';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+
+  // on mappe le retour du Reducer 'UsersReducer' dans la variable 'users'
+  users: UsersReducer
+  
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_users.js
@@ -0,0 +1,12 @@
+/*
+Un Reducer est une fonction qui retourne un objet qui est une partie du state de l'application 
+*/
+
+export default function(){
+    return [
+        {nom: 'Sarah Croche'},
+        {nom: 'Eléonore Labanquise'},
+        {nom: 'Emma Karena'},
+        {nom: 'Jean Bonnot'},
+    ]
+}
