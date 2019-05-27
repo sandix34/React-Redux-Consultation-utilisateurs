0d9a3c507c1c244f269909f059866c5f65a25939
sandrine <sandrinezocca@gmail.com>
2019-05-27 18:09:19 +0200

    envoie et dispatch de l'action

--- src/containers/user_list.js
+++ src/containers/user_list.js
@@ -1,38 +1,49 @@
 // les composants qui ont besoin de connaître les changements des morceaux de state sont des containers
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
 // import de la fonction "connect" pour connecter react à redux
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+
+// appel de l'action creator
+import { selectUser } from "../actions/index";
+import { bindActionCreators } from "redux";
 
 class UserList extends Component {
-  
-    render() { 
-        return ( 
-            <div>
-                <ul>
-                    {
-                        this.props.myUsers.map((user) => {
-                            return (
-                                <li key={user.id}>
-                                    {user.name}
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </div>
-         );
-    }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.myUsers.map(user => {
+            return (
+              <li key={user.id} onClick={() => this.props.selectUser(user)}>
+                {user.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
- 
+
 // fonction appelée à chaque fois que le state change
 /*
 le reducer va retourner le state, on récupère la partie qui nous intéresse du state => state.users
 et on va la mettre dans "myUsers"
 */
-function mapStateToProps(state){
-    return {
-      myUsers:  state.users
-    }
+function mapStateToProps(state) {
+  return {
+    myUsers: state.users
+  };
 }
+
+// envoie et dispatche l'action aux reducers
+function mapDispatchToProps(dispatch) {
+  bindActionCreators({ selectUser: selectUser }, dispatch);
+}
+
 // connection du composant container à Redux
-export default connect(mapStateToProps)(UserList);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserList);
